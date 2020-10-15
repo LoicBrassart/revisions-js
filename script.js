9894a09c7e1928507d291013b3ca24ee26ad8658
LoicBrassart <loic.brassart@gmail.com>
2020-10-15 14:33:53 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//const btn = document.getElementById("btn-rand");
+const btn = document.querySelector("#btn-rand");
+const stranger = document.getElementById("stranger");
+
+btn.addEventListener("click", ()=>{
+    stranger.innerHTML = "To<span class='blue'>to</span>";
+} );
+
+
+// function toto() {};
+// const toto = () => {};
