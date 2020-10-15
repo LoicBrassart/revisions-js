550691ceeae35db70490d9d87dc3fccb5e05ccc9
LoicBrassart <loic.brassart@gmail.com>
2020-10-15 15:02:36 +0200

    Added code snippet about if statements and ternaries

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ btn.addEventListener("click", ()=>{
     stranger.innerHTML = "To<span class='blue'>to</span>";
 } );
 
+const toto = document.querySelector("#toto");
+toto.innerText = "?";
+
+if(stranger.innerText === "world") {
+    toto.innerText = "monde";
+} else {
+    toto.innerText = "Je ne sais pas";
+}
+
+//toto.innerText = stranger.innerText === "world" ?"monde":"Je ne sais pas";
 
 // function toto() {};
 // const toto = () => {};
